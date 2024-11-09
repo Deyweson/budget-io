84288652e41d937490ae0c68ba36c9e70b31eae2
Deyweson <deywesonalm@gmail.com>
2024-11-09 19:19:10 -0300

    :wrench: Config: Criação da pasta pdf dinamicamente

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import app from './app'
+import fs from 'node:fs'
 
 const port = 3000
 
@@ -7,4 +8,14 @@ app.listen({
   host: '0.0.0.0'
 }, () => {
   console.log(`Server is running at port ${port}`)
+
+  if (!fs.existsSync('./pdf')) {
+    fs.mkdir('./pdf', (err) => {
+      if (err) {
+        console.log("Deu ruim...");
+        return
+      }
+      console.log("Diretório criado! =)")
+    });
+  }
 })
--- src/services/create-pdf.ts
+++ src/services/create-pdf.ts
@@ -3,23 +3,23 @@ require('dotenv').config()
 
 export async function GeneratePDF(html: string): Promise<Uint8Array> {
   const browser = await puppeteer.launch({
-    args: [
+    args: process.env.NODE_ENV === 'production' ? [
       "--disable-setuid-sandbox",
       "--no-sandbox",
       "--single-process",
       "--no-zygote"
-    ],
+    ] : [],
     executablePath: process.env.NODE_ENV === 'production' ? process.env.PUPPETEER_EXECUTABLE_PATH : puppeteer.executablePath()
-  });
-  const page = await browser.newPage();
+  })
+  const page = await browser.newPage()
 
-  await page.setContent(html, { waitUntil: 'networkidle0' });
+  await page.setContent(html, { waitUntil: 'networkidle0' })
 
   const pdf = await page.pdf({
     format: 'A4',
     printBackground: true
-  });
+  })
 
-  await browser.close();
-  return pdf;
+  await browser.close()
+  return pdf
 }
