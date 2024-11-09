6049d09f2cb2b74f5f5a63ad695c9492addc1b61
Deyweson <deywesonalm@gmail.com>
2024-11-09 18:56:08 -0300

    :wrench: Config: Host no server.ts

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,9 @@ import app from './app'
 
 const port = 3000
 
-app.listen({port}, () => {
+app.listen({
+  port,
+  host: '0.0.0.0'
+}, () => {
   console.log(`Server is running at port ${port}`)
 })
